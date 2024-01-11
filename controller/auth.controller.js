780ef358d7019bbd427c3bf5e9defd4235b71f72
semojan <97410424+semojan@users.noreply.github.com>
2024-01-11 12:10:36 +0330

    login-signup-routes-added

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -16,11 +16,25 @@ router.get("/signup", function(req, res, next){
         }
     }
 
-    res.render("", inputData = sessionData);
+    res.render("signup", inputData = sessionData);
 });
 
 // router.post("signup", function(req, res, next){
 
 // });
+
+router.get("/login", function(req, res, next){
+    let sessionData = sessionFlash.getSessionData(req);
+
+    if (!sessionData){
+        sessionData = {
+            username: "",
+            password: "",
+            email: ""
+        }
+    }
+
+    res.render("login", inputData = sessionData);
+});
 
 module.exports = router;
